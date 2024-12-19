985c19ff28188798e1bfd538ad4245e21ef7b263
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-12-19 13:04:11 +0530

    frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import Home from "./components/Home"
+export default function App() {
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
